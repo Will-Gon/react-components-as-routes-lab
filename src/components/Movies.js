48446c316547940c606499a62b4f45fa52a4dbcc
Will-Gon <will.colocho@gmail.com>
2021-01-06 14:42:40 -0800

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,11 +2,27 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
-  return (
+    return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+      return <div>
+              <h3>
+                Name: {movie.title}
+              </h3>
+              <br/>
+                Time: {movie.time}
+              <br/>
+              <br/>
+                Genres:
+              <ul>{movie.genres.map(genre => {
+                return <li>{genre}</li>
+              })}</ul>
+            </div>
+
+        })}
     </div>
-  );
+    )
 };
 
 export default Movies;
